8a75f8a89178cb06e4e59d0058b06271ed7993f1
john lam <john.lam@nokia.com>
2020-06-30 20:37:09 -0500

    capv helm

--- govcvars.sh
+++
@@ -1,8 +0,0 @@
-export GOVC_INSECURE=1 # Don't verify SSL certs on vCenter
-export GOVC_URL=10.46.83.23 # vCenter IP/FQDN
-export GOVC_USERNAME=administrator@vsphere.local # vCenter username
-export GOVC_PASSWORD=DCLab_123!!
-export GOVC_DATACENTER=US_DC
-export GOVC_DATASTORE=ISCSI-PURE-LUN-4 #Default datastore to deploy to
-export GOVC_NETWORK="VLAN-703" # Default network to deploy to
-export GOVC_RESOURCE_POOL='k8s' # Default resource pool to deploy to
